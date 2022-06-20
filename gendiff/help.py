1fb8372399d36e43d5b236b25c5d1bd44c00f959
Svet Ivanov <svetoslav.bc@gmail.com>
2022-06-20 06:47:33 +0300

    my_first_step

---
+++ gendiff/help.py
@@ -0,0 +1,9 @@
+import argparse
+
+def get_parsing():
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file',)
+    parser.add_argument('second_file',)
+
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+from gendiff.help import get_parsing
+def main():
+    get_parsing()
+
+if __name__ == '__main__':
+    main()
