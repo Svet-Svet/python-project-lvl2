08afe47d52798a314ceba6ed3ac84289aac83704
Svet Ivanov <svetoslav.bc@gmail.com>
2022-06-26 17:49:05 +0300

    my_3_step-2

--- gendiff/scripts/comparsion.py
+++ gendiff/scripts/comparsion.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
+import os
+import sys
 from gendiff.comprasion.comparison import generate_diff
 
+
+def abs_path(path):
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
 
 def main():
     generate_diff('file1', 'file2')
 
 if __name__ == '__main__':
-    main()
+    print(list(map(abs_path, sys.argv[1:0])))
