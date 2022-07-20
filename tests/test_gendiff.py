23ac08a6a42994d2a73e33e39899ea90072ad5d7
Svet Ivanov <svetoslav.bc@gmail.com>
2022-07-20 22:13:16 +0300

    my_4_step-5

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -11,5 +11,5 @@ file2 = {
 
 
 def test_gendiff_good():
-    assert generate_diff(file1, file2) == '{  timeout: 50 }'
+    assert generate_diff("tests/fixtures/file1.json", "tests/fixtures/file2.json") == '{  timeout: 50 }'
 
