1bf002d1ee8acaff087c17b0390e1cff7edfdda3
Svet Ivanov <svetoslav.bc@gmail.com>
2022-08-13 21:03:54 +0300

    my_6_step_3

--- gendiff/comparison/comparison.py
+++ gendiff/comparison/comparison.py
@@ -1,8 +1,9 @@
 # from gendiff.comparison.formatters.get_format import get_format
 from gendiff.comparison.formatters.stylish import stylish
 
-SIGN_NEW_DATA = '+'
-SIGN_OLD_DATA = '-'
+ADDED = '+'
+REMOVED = '-'
+IDENTICAL = ' '
 
 
 def value_to_string(value):
@@ -11,26 +12,38 @@ def value_to_string(value):
     return value
 
 
-def generate_diff(file1, file2, format='stylish'):
+def get_diff_graph(file1, file2, format='stylish'):
     merged_dict = file1 | file2
     sorted_tuple = dict(sorted(merged_dict.items(), key=lambda x: x[0]))
-    result = ''
+    diff_graph = []
     for key in sorted_tuple.keys():
         value1 = value_to_string(file1.get(key))
         value2 = value_to_string(file2.get(key))
+
+        if isinstance(value1, dict) and isinstance(value2, dict):
+            sub_graph = generate_diff(value1, value2)
+            diff_graph.append((IDENTICAL, key, sub_graph))
+
         if key not in file1.keys():
-            result = f'{result}  {SIGN_NEW_DATA} {key}: {stylish(value2)}\n'
+            diff_graph.append(('+', key, value2))
         else:
             if key not in file2.keys():
-                result = f'{result}  {SIGN_OLD_DATA} {key}: {stylish(value1)}\n'
+                diff_graph.append((REMOVED, key, value1))
             else:
                 if file1.get(key) == file2.get(key):
-                    result = f'{result}    {key}: {stylish(value1)}\n'
+                    diff_graph.append((IDENTICAL, key, value1))
                 else:
-                    result = f'{result}  {SIGN_OLD_DATA} {key}: {stylish(value1)}\n'
-                    result = f'{result}  {SIGN_NEW_DATA} {key}: {stylish(value1)}\n'
-    print(f'{{\n{result}}}')
-    return f'{{\n{result}}}'
+                    diff_graph.append((REMOVED, key, value1))
+                    diff_graph.append((ADDED, key, value2))
+    # print(diff_graph)
+    return diff_graph
+
+
+def generate_diff(file1, file2, format='stylish'):
+    graph = get_diff_graph(file1, file2)
+    #formatter = FORMATTERS[format]
+    print(stylish(graph))
+    return stylish(graph)
 
 
 # def generate_diff(file1, file2):
--- gendiff/comparison/formatters/stylish.py
+++ gendiff/comparison/formatters/stylish.py
@@ -1,12 +1,29 @@
-def stylish(node, replacer=' ', spaces_count=3, _deep=0):
-    if type(node) is dict:
-        result = ''
-        parenthesis_start = "{"
-        parenthesis_end = "}"
-        count_spaces = replacer * spaces_count * (_deep + 1)
-        quote_spaces = replacer * spaces_count * _deep
+# def stylish(node, replacer=' ', spaces_count=3, _deep=0):
+#     if type(node) is dict:
+#         result = ''
+#         parenthesis_start = "{"
+#         parenthesis_end = "}"
+#         count_spaces = replacer * spaces_count * (_deep + 1)
+#         quote_spaces = replacer * spaces_count * _deep
+#
+#         for key, vallue in node.items():
+#             result += f'\n{count_spaces}{key}: {stylish(vallue, replacer, spaces_count, _deep + 1)}'
+#         return f'{parenthesis_start}{result}\n{quote_spaces}{parenthesis_end}'
+#     return str(node)
 
-        for key, vallue in node.items():
-            result += f'\n{count_spaces}{key}: {stylish(vallue, replacer, spaces_count, _deep + 1)}'
-        return f'{parenthesis_start}{result}\n{quote_spaces}{parenthesis_end}'
-    return str(node)
+
+def stylish(graph, replacer=' ', spaces_count=3, _deep=0):
+    result = ''
+    parenthesis_start = "{"
+    parenthesis_end = "}"
+    count_spaces = replacer * spaces_count * (_deep + 1)
+    quote_spaces = replacer * spaces_count * _deep
+    for node in graph:
+        if isinstance(node[2], dict):
+            result += stylish(node[2])
+            # result += f'\n{count_spaces}{key}: {stylish(vallue, replacer, spaces_count, _deep + 1)}
+        else:
+            # out += '\n' + ' '.join(node)
+            result += f'\n {node}'
+    return result
+
