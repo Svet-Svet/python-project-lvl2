d49af5e4485913382d09cb8d7f1ec803ac57a1fc
Svet Ivanov <svetoslav.bc@gmail.com>
2022-09-07 23:19:04 +0300

    refactoring tests

--- gendiff/comparison/formatters/stylish.py
+++ gendiff/comparison/formatters/stylish.py
@@ -1,28 +1,3 @@
-# АЛЬТЕРНАТИВНАЯ ФУНКЦИЯ
-# def stylish(graph, replacer=' ', spaces_count=1, _deep=0):
-#     result = ''
-#     parenthesis_start = "{"
-#     parenthesis_end = "}"
-#     count_spaces = replacer * spaces_count * (_deep + 1)
-#     quote_spaces = replacer * spaces_count * _deep
-#     for node in graph:
-#         if isinstance(node, list):
-#             result += stylish(node, replacer, spaces_count, _deep + 1)
-#             # result += f'\n{count_spaces}{key}: {stylish(value, replacer, spaces_count, _deep + 1)}
-#         else:
-#             for key, val in sorted(node.items()):
-#                 # if node == '-' or node == '-' or node == '+':
-#                 if key == 'status':
-#                     # result += f'{count_spaces}{node} '
-#                     result += f'{count_spaces}{val} '
-#                 elif key == 'key':
-#                     result += f'{val}: '
-#                 elif key == 'value':
-#                     result += f'{val}'
-#             # result += f'{count_spaces}{node}'
-#             # count_spaces += " "
-#     return f'{parenthesis_start}\n {result}\n{quote_spaces}{parenthesis_end}'
-
 
 def stylish(graph, replacer=' ', spaces_count=1, _deep=0):
     result = ''
@@ -33,19 +8,19 @@ def stylish(graph, replacer=' ', spaces_count=1, _deep=0):
     for node in graph:
         if isinstance(node[2], list):
             if node[0] == "added":
-                general_names_of_group = f'\n{count_spaces}+ {node[1]}: '
-                result += general_names_of_group + stylish(node[2], replacer, spaces_count, _deep + 4)
+                keys = f'\n{count_spaces}+ {node[1]}: '
+                result += keys + stylish(node[2], replacer, spaces_count, _deep + 4)
             elif node[0] == "removed":
-                general_names_of_group = f'\n{count_spaces}- {node[1]}: '
-                result += general_names_of_group + stylish(node[2], replacer, spaces_count, _deep + 4)
+                keys = f'\n{count_spaces}- {node[1]}: '
+                result += keys + stylish(node[2], replacer, spaces_count, _deep + 4)
             elif node[0] == "identical":
-                general_names_of_group = f'\n{count_spaces}  {node[1]}: '
-                result += general_names_of_group + stylish(node[2], replacer, spaces_count, _deep + 4)
+                keys = f'\n{count_spaces}  {node[1]}: '
+                result += keys + stylish(node[2], replacer, spaces_count, _deep + 4)
             elif node[0] == "changed":
-                general_names_of_group_old = f'\n{count_spaces}- {node[1]}: '
-                general_names_of_group_new = f'\n{count_spaces}+ {node[1]}: {node[3]}'
-                result += general_names_of_group_old + stylish(node[2], replacer, spaces_count, _deep + 4)
-                result += general_names_of_group_new
+                keys_old = f'\n{count_spaces}- {node[1]}: '
+                keys_new = f'\n{count_spaces}+ {node[1]}: {node[3]}'
+                result += keys_old + stylish(node[2], replacer, spaces_count, _deep + 4)
+                result += keys_new
 
         elif node[0] == "added":
             result += f'\n{count_spaces}+ {node[1]}: {node[2]} '
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,8 +1,10 @@
-import gendiff
+from gendiff.comparison.formatters.stylish import stylish
+from gendiff.comparison.formatters.plain import plain
+from gendiff.comparison.formatters.json import json_format
 
 
-def test_gendiff_json():
-    assert gendiff.generate_diff_json("tests/fixtures/file1.json", "tests/fixtures/file2.json") == '''{
+def test_json():
+    assert json_format("tests/fixtures/file1.json") == '''{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
@@ -12,19 +14,8 @@ def test_gendiff_json():
 }'''
 
 
-def test_gendiff_yaml():
-    assert gendiff.generate_diff_yaml("tests/fixtures/file1.yaml", "tests/fixtures/file2.yaml") == '''{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}'''
-
-
-def test_gendiff_stylish():
-    assert gendiff.stylish("tests/fixtures/file_deep1.json", "tests/fixtures/file_deep2.json") == '''{
+def test_stylish():
+    assert stylish("tests/fixtures/file_deep1.json", "tests/fixtures/file_deep2.json") == '''{
     common: {
       + follow: false
         setting1: Value 1
@@ -70,44 +61,8 @@ def test_gendiff_stylish():
 }'''
 
 
-def test_gendiff_deep_yaml():
-    assert gendiff.generate_diff_yaml("tests/fixtures/file_deep1.yaml", "tests/fixtures/file_deep2.yaml") == '''{
-    common:
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5:
-            key5: value5
-        setting6:
-            doge:
-              - wow:
-              + wow: so much
-            key: value
-          + ops: vops
-    group1:
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest:
-            key: value
-      + nest: str
-  - group2:
-        abc: 12345
-        deep:
-            id: 45
-  + group3:
-        deep:
-            id:
-                number: 45
-        fee: 100500
-}'''
-
-
-def test_gendiff_plain():
-    assert gendiff.plain("tests/fixtures/file_deep1.json", "tests/fixtures/file_deep2.json") == '''{
+def test_plain():
+    assert plain("tests/fixtures/file_deep1.json", "tests/fixtures/file_deep2.json") == '''{
 Property 'common.follow' was added with value: false
 Property 'common.setting2' was removed
 Property 'common.setting3' was updated. From true to null
