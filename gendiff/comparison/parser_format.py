dc7b3dcb6ad1f00b6ee2ac9178117a7a7f330fa8
Svet Ivanov <svetoslav.bc@gmail.com>
2022-09-13 14:08:09 +0300

    refactoring refactoring yaml-mod for hexlet_tests

--- gendiff/comparison/parser_format.py
+++ gendiff/comparison/parser_format.py
@@ -1,14 +1,14 @@
 import json
 import yaml
-import os.path
 
 
 def generate_parser_format(file):
-    if os.path.splitext(file[-4:]) == 'json':
+    if file.endswith('.json'):
         with open(file, 'r') as file:
             file = json.load(file)
             return file
-    else:
+    elif file.endswith('.yml') or file.endswith('.yaml'):
         with open(file, 'r') as file:
             file = yaml.safe_load(file)
             return file
+    raise Exception('Invalid file format.')
