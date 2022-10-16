cb698d06fc1e09652bc7d1f4421624d700c90d1d
Svet Ivanov <svetoslav.bc@gmail.com>
2022-10-16 22:35:05 +0300

    fixed_project — refactoring test_fixtures

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,76 +2,18 @@ from gendiff.comparison.formatters.stylish import stylish
 from gendiff.comparison.formatters.plain import plain
 from gendiff.comparison.formatters.json import json_format
 
+from tests.fixtures.result_json import result_json
+from tests.fixtures.result_stylish import result_stylish
+from tests.fixtures.result_plain import result_plain
+
 
 def test_json():
-    assert json_format("tests/fixtures/file1.json") == '''{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}'''
+    assert json_format("tests/fixtures/file1.json") == result_json()
 
 
 def test_stylish():
-    assert stylish("tests/fixtures/file_deep1.json", "tests/fixtures/file_deep2.json") == '''{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow:
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}'''
+    assert stylish("tests/fixtures/file_deep1.json", "tests/fixtures/file_deep2.json") == result_stylish()
 
 
 def test_plain():
-    assert plain("tests/fixtures/file_deep1.json", "tests/fixtures/file_deep2.json") == '''{
-Property 'common.follow' was added with value: false
-Property 'common.setting2' was removed
-Property 'common.setting3' was updated. From true to null
-Property 'common.setting4' was added with value: 'blah blah'
-Property 'common.setting5' was added with value: [complex value]
-Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
-Property 'common.setting6.ops' was added with value: 'vops'
-Property 'group1.baz' was updated. From 'bas' to 'bars'
-Property 'group1.nest' was updated. From [complex value] to 'str'
-Property 'group2' was removed
-Property 'group3' was added with value: [complex value]
-}'''
+    assert plain("tests/fixtures/file_deep1.json", "tests/fixtures/file_deep2.json") == result_plain()
