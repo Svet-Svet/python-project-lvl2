b620f84676dd01f522303b32cadb67df54507408
Svet Ivanov <svetoslav.bc@gmail.com>
2022-10-27 20:03:51 +0300

    fixed_project — fixed parser_format_2

--- gendiff/comparison/parser_format.py
+++ gendiff/comparison/parser_format.py
@@ -3,11 +3,20 @@ import yaml
 
 
 def generate_parser_format(file):
-    with open(file, 'r') as file:
-        if file.endswith('.json'):
-            file_path = json.load(file)
-            return file_path
-        elif file.endswith('.yml') or file.endswith('.yaml'):
-            file_path = yaml.safe_load(file)
-            return file_path
-        raise Exception('Invalid file format.')
+    if file.endswith('.json'):
+        with open(file, 'r') as file:
+            return json.load(file)
+    elif file.endswith('.yml') or file.endswith('.yaml'):
+        with open(file, 'r') as file:
+            return yaml.safe_load(file)
+    raise Exception('Invalid file format.')
+
+# def generate_parser_format(file):
+#     with open(file, 'r') as file:
+#         if file.endswith('.json'):
+#             file_path = json.load(file)
+#             return file_path
+#         elif file.endswith('.yml') or file.endswith('.yaml'):
+#             file_path = yaml.safe_load(file)
+#             return file_path
+#         raise Exception('Invalid file format.')
