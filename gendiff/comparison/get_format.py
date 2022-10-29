3b024439c9d7f26442bec26de2d49524a014153c
Svet Ivanov <svetoslav.bc@gmail.com>
2022-10-29 18:43:43 +0300

    fixed_project — fixed parser_format and get_format

--- gendiff/comparison/get_format.py
+++ gendiff/comparison/get_format.py
@@ -3,10 +3,21 @@ from gendiff.comparison.formatters.plain import plain
 from gendiff.comparison.formatters.json import json_format
 
 
-def get_format(tree, formatter):
-    if formatter == 'stylish':
-        return stylish(tree)
-    elif formatter == 'plain':
-        return plain(tree)
-    elif formatter == 'json':
-        return json_format(tree)
+FORMATTERS = {
+    'stylish': stylish,
+    'plain': plain,
+    'json': json_format
+}
+
+
+def get_format(tree, formats='stylish'):
+    formatter = FORMATTERS[formats]
+    return formatter(tree)
+
+# def get_format(tree, formatter):
+#     if formatter == 'stylish':
+#         return stylish(tree)
+#     elif formatter == 'plain':
+#         return plain(tree)
+#     elif formatter == 'json':
+#         return json_format(tree)
--- gendiff/comparison/parser_format.py
+++ gendiff/comparison/parser_format.py
@@ -2,11 +2,10 @@ import json
 import yaml
 
 
-def generate_parser_format(file):
-    if file.endswith('.json'):
-        with open(file, 'r') as file:
-            return json.load(file)
-    elif file.endswith('.yml') or file.endswith('.yaml'):
-        with open(file, 'r') as file:
-            return yaml.safe_load(file)
-    raise Exception('Invalid file format.')
+def generate_parser_format(file_path):
+    with open(file_path, 'r') as _f:
+        if file_path.endswith('.json'):
+            return json.load(_f)
+        elif file_path.endswith('.yml') or file_path.endswith('.yaml'):
+            return yaml.safe_load(_f)
+        raise Exception('Invalid file format.')
