5b3c89b7072ef0472c29d10100bd3ba8aae7f8be
Svet Ivanov <svetoslav.bc@gmail.com>
2022-10-29 20:00:56 +0300

    fixed_project — fixed stylish_2

--- gendiff/comparison/formatters/stylish.py
+++ gendiff/comparison/formatters/stylish.py
@@ -12,39 +12,40 @@ def stylish(graph, _deep=0):
     parenthesis_end = "}"
     count_spaces = replacer * (_deep + 2)
     quote_spaces = replacer * _deep
-    for node in graph:
+    for node_general in graph:
+        node = is_bool(node_general)
         status, key, *values = node
         if isinstance(node[2], list):
             if status == ADDED:
-                keys = f'\n{count_spaces}+ {is_bool(node[1])}: '
-                result += keys + stylish(is_bool(node[2]), _deep + 4)
+                keys = f'\n{count_spaces}+ {node[1]}: '
+                result += keys + stylish(node[2], _deep + 4)
             elif status == REMOVED:
-                keys = f'\n{count_spaces}- {is_bool(node[1])}: '
-                result += keys + stylish(is_bool(node[2]), _deep + 4)
+                keys = f'\n{count_spaces}- {node[1]}: '
+                result += keys + stylish(node[2], _deep + 4)
             elif status == IDENTICAL:
-                keys = f'\n{count_spaces}  {is_bool(node[1])}: '
-                result += keys + stylish(is_bool(node[2]), _deep + 4)
+                keys = f'\n{count_spaces}  {node[1]}: '
+                result += keys + stylish(node[2], _deep + 4)
             elif status == CHANGED:
-                old = f'\n{count_spaces}- {is_bool(node[1])}: '
-                new = f'\n{count_spaces}+ {is_bool(node[1])}: {is_bool(node[3])}'
-                result += old + stylish(is_bool(node[2]), _deep + 4)
+                old = f'\n{count_spaces}- {node[1]}: '
+                new = f'\n{count_spaces}+ {node[1]}: {node[3]}'
+                result += old + stylish(node[2], _deep + 4)
                 result += new
 
         elif status == CHANGED and isinstance(node[3], list):
-                old = f'\n{count_spaces}- {is_bool(node[1])}: {is_bool(node[2])}'
-                new = f'\n{count_spaces}+ {is_bool(node[1])}: '\
-                      + stylish(is_bool(node[3]), _deep + 4)
+                old = f'\n{count_spaces}- {node[1]}: {node[2]}'
+                new = f'\n{count_spaces}+ {node[1]}: '\
+                      + stylish(node[3], _deep + 4)
                 result += old
                 result += new
         elif status == ADDED:
-            result += f'\n{count_spaces}+ {is_bool(node[1])}: {is_bool(node[2])}'
+            result += f'\n{count_spaces}+ {node[1]}: {node[2]}'
         elif status == REMOVED:
-            result += f'\n{count_spaces}- {is_bool(node[1])}: {is_bool(node[2])}'
+            result += f'\n{count_spaces}- {node[1]}: {node[2]}'
         elif status == IDENTICAL:
-            result += f'\n{count_spaces}  {is_bool(node[1])}: {is_bool(node[2])}'
+            result += f'\n{count_spaces}  {node[1]}: {node[2]}'
         elif status == CHANGED:
-            result += f'\n{count_spaces}- {is_bool(node[1])}: {is_bool(node[2])}'
-            result += f'\n{count_spaces}+ {is_bool(node[1])}: {is_bool(node[3])}'
+            result += f'\n{count_spaces}- {node[1]}: {node[2]}'
+            result += f'\n{count_spaces}+ {node[1]}: {node[3]}'
     return f'{parenthesis_start}{result}\n{quote_spaces}{parenthesis_end}'
 
 
--- gendiff/comparison/parser_format.py
+++ gendiff/comparison/parser_format.py
@@ -3,9 +3,9 @@ import yaml
 
 
 def generate_parser_format(file_path):
-    with open(file_path, 'r') as _f:
+    with open(file_path, 'r') as f:
         if file_path.endswith('.json'):
-            return json.load(_f)
+            return json.load(f)
         elif file_path.endswith('.yml') or file_path.endswith('.yaml'):
-            return yaml.safe_load(_f)
+            return yaml.safe_load(f)
         raise Exception('Invalid file format.')
