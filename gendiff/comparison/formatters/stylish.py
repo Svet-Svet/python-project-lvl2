7a37684bba801d2392f807b8848d03190014d85f
Svet Ivanov <svetoslav.bc@gmail.com>
2022-10-29 19:44:00 +0300

    fixed_project — fixed stylish

--- gendiff/comparison/formatters/stylish.py
+++ gendiff/comparison/formatters/stylish.py
@@ -13,37 +13,45 @@ def stylish(graph, _deep=0):
     count_spaces = replacer * (_deep + 2)
     quote_spaces = replacer * _deep
     for node in graph:
-        status = node[0]
+        status, key, *values = node
         if isinstance(node[2], list):
             if status == ADDED:
-                keys = f'\n{count_spaces}+ {node[1]}: '
-                result += keys + stylish(node[2], _deep + 4)
+                keys = f'\n{count_spaces}+ {is_bool(node[1])}: '
+                result += keys + stylish(is_bool(node[2]), _deep + 4)
             elif status == REMOVED:
-                keys = f'\n{count_spaces}- {node[1]}: '
-                result += keys + stylish(node[2], _deep + 4)
+                keys = f'\n{count_spaces}- {is_bool(node[1])}: '
+                result += keys + stylish(is_bool(node[2]), _deep + 4)
             elif status == IDENTICAL:
-                keys = f'\n{count_spaces}  {node[1]}: '
-                result += keys + stylish(node[2], _deep + 4)
+                keys = f'\n{count_spaces}  {is_bool(node[1])}: '
+                result += keys + stylish(is_bool(node[2]), _deep + 4)
             elif status == CHANGED:
-                old = f'\n{count_spaces}- {node[1]}: '
-                new = f'\n{count_spaces}+ {node[1]}: {node[3]}'
-                result += old + stylish(node[2], _deep + 4)
+                old = f'\n{count_spaces}- {is_bool(node[1])}: '
+                new = f'\n{count_spaces}+ {is_bool(node[1])}: {is_bool(node[3])}'
+                result += old + stylish(is_bool(node[2]), _deep + 4)
                 result += new
 
         elif status == CHANGED and isinstance(node[3], list):
-                old = f'\n{count_spaces}- {node[1]}: {node[2]}'
-                new = f'\n{count_spaces}+ {node[1]}: '\
-                      + stylish(node[3], _deep + 4)
+                old = f'\n{count_spaces}- {is_bool(node[1])}: {is_bool(node[2])}'
+                new = f'\n{count_spaces}+ {is_bool(node[1])}: '\
+                      + stylish(is_bool(node[3]), _deep + 4)
                 result += old
                 result += new
         elif status == ADDED:
-            result += f'\n{count_spaces}+ {node[1]}: {node[2]}'
+            result += f'\n{count_spaces}+ {is_bool(node[1])}: {is_bool(node[2])}'
         elif status == REMOVED:
-            result += f'\n{count_spaces}- {node[1]}: {node[2]}'
+            result += f'\n{count_spaces}- {is_bool(node[1])}: {is_bool(node[2])}'
         elif status == IDENTICAL:
-            result += f'\n{count_spaces}  {node[1]}: {node[2]}'
+            result += f'\n{count_spaces}  {is_bool(node[1])}: {is_bool(node[2])}'
         elif status == CHANGED:
-            result += f'\n{count_spaces}- {node[1]}: {node[2]}'
-            result += f'\n{count_spaces}+ {node[1]}: {node[3]}'
-    return f'{parenthesis_start}{result}\n{quote_spaces}{parenthesis_end}'.replace('True', 'true')\
-        .replace('False', 'false').replace('None', 'null')
+            result += f'\n{count_spaces}- {is_bool(node[1])}: {is_bool(node[2])}'
+            result += f'\n{count_spaces}+ {is_bool(node[1])}: {is_bool(node[3])}'
+    return f'{parenthesis_start}{result}\n{quote_spaces}{parenthesis_end}'
+
+
+def is_bool(item):
+    if type(item) is bool:
+        return str(item).lower()
+    elif item is None:
+        return 'null'
+    else:
+        return item
